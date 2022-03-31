4a307e9f9358fca0721e104a3164bc7fdfe2f6bb
Avi12121212 <102717737+Avi12121212@users.noreply.github.com>
2022-03-31 14:56:29 +0530

    Initial commit

---
+++ add.py
@@ -0,0 +1,18 @@
+a=int( input("Enter A\n"))
+b=int( input("Enter B\n"))
+total=a+b
+print(a,b,total)
+# SIngle line comment
+"""
+* multiply
+/ divide
+// integer division
+** to the power. 2**3 =8
++ plus
+- minus
+% remainder 10 % 3 =1
+
+"""
+'''
+
+'''
